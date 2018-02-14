15ad4bf1a541713476b5a93f6c32e74bae1991c8
Philip <philiplouietemplonuevo@yahoo.ca>
2018-02-14 19:22:26 +0000

    Character counting program

---
+++ countLetter.js
@@ -0,0 +1,15 @@
+function countLetter(original) {
+  var copy = original.replace(/\s/g,'').split("");
+  var countLet = {};
+
+  for (var i = 0; i < copy.length; i++) {
+    if (copy[i] in countLet) {
+      countLet[copy[i]]++
+    } else {
+      countLet[copy[i]] = 1
+    }
+  }
+  return countLet;
+}
+
+console.log(countLetter("lighthouse in the house"));
